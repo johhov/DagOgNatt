41b291d0c6d39ba8ca02ec1ef82f461943b87eab
Ørjan Røkkum Brandtzæg <orod_sul@hotmail.com>
2013-01-27 14:40:58 +0100

    salvaged files

---
+++ Dag og Natt/Dag og Natt/Dag og Natt/Model/Flower.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Xna.Framework;
+
+
+namespace Dag_og_Natt 
+{
+	class Flower : Movable
+	{
+		public bool watered;
+
+	public void Update()
+	{
+	if ((passableAtDay && Global.day) || (passableAtNight && !Global.day))
+            {
+                passable = true;
+            }
+            else
+            {
+                passable = false;
+            }
+	if (watered && !Global.day)
+		{
+		textureDay = TextureNight;
+		}
+
+            this.position.X = startingPosition.X - Global.offset;
+		}
+		public Flower(Vector2 position, bool passableAtDay, bool passableAtNight, Rectangle currentAnimation)
+		: base(position, passableAtDay, passableAtNight, currentAnimation)
+		{
+			watered = false;
+		}
+	}
+}
